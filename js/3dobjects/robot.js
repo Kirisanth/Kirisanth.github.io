cc594aadb79a1a57fc67e467318c1dfc29366c2b
kirisanth <kirisanth.s@gmail.com>
2014-11-25 01:14:19 -0500

    A primitive robot using three.js

---
+++ js/3dobjects/robot.js
@@ -0,0 +1,137 @@
+"use strict";
+
+//Script requires three.js library
+
+// The MIT License (MIT)
+
+// Copyright (c) 2014 Kirisanth
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+var Robot = function(firstName) {
+	this.firstName = firstName;
+	this.init();
+}
+
+Robot.prototype.position = 0;
+Robot.prototype.init = function() {
+	this.body = this.buildBody();
+
+	this.leftArm = this.buildArms(0.5,0,0);
+	this.rightArm = this.buildArms(-0.5,0,0);
+
+	this.leftLeg = this.buildLegs(0.5,-0.5,0);
+	this.rightLeg = this.buildLegs(-0.5,-0.5,0);
+
+	this.head = this.buildHead(0,1,0);
+
+	this.body.add(this.leftArm);
+	this.body.add(this.rightArm);
+	this.body.add(this.leftLeg);
+	this.body.add(this.rightLeg);
+	this.body.add(this.head);
+
+	scene.add(this.body);
+}
+
+Robot.prototype.walkMultipler = 1;
+Robot.prototype.walk = function() {
+	console.log("I am walking!");
+	var m = new THREE.Matrix4();
+
+	var movementBound = 0.1;
+
+	if(this.position > movementBound) {
+		this.walkMultipler = -1
+	}
+
+	if(this.position < -1*movementBound) {
+		this.walkMultipler = 1
+	}	
+
+	this.position += this.walkMultipler*0.01;
+
+	m.makeTranslation(0,0,this.position);
+
+	this.rightLeg.applyMatrix(m);
+
+	m.makeTranslation(0,0,-1*this.position);
+
+	this.leftLeg.applyMatrix(m);
+}
+
+Robot.prototype.sayHello = function() {
+	console.log("Hello, I'm " + this.firstName);
+}
+
+Robot.prototype.buildBody = function() {
+	var m = new THREE.Matrix4();
+
+	return this.cube(1,1,1,0x00ff00);
+}
+
+Robot.prototype.buildArms = function(x,y,z) {
+	var m = new THREE.Matrix4();
+	var sphere = this.sphere(0.3,32,32);
+
+	m.makeTranslation(x,y,z);
+
+	sphere.applyMatrix(m);
+
+	return sphere;
+}
+
+Robot.prototype.buildLegs = function(x,y,z) {
+	var m = new THREE.Matrix4();
+	var sphere = this.sphere(0.3,32,32);
+
+	m.makeTranslation(x,y,z);
+
+	sphere.applyMatrix(m);
+
+	return sphere;
+}
+
+Robot.prototype.buildHead = function(x,y,z) {
+	var m = new THREE.Matrix4();
+	var sphere = this.sphere(0.5,32,32);
+
+	m.makeTranslation(x,y,z);
+
+	sphere.applyMatrix(m);
+
+	return sphere;
+}
+
+// radius, widthSegment, heightSegment
+Robot.prototype.sphere = function(r,w,h) {
+	var sphereGeometry = new THREE.SphereGeometry(r,w,h);
+	var sphereMaterial = new THREE.MeshBasicMaterial({color:0xffff00});
+	var sphere = new THREE.Mesh(sphereGeometry,sphereMaterial);
+
+	return sphere
+}
+
+Robot.prototype.cube = function(x,y,z,color) {
+	var cubeGeometry = new THREE.BoxGeometry(x, y, z);
+	var cubeMaterial = new THREE.MeshBasicMaterial( { color: color } );
+	var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+	return cube;
+}
--- js/main.js
+++ js/main.js
@@ -9,41 +9,63 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.getElementById('canvas').appendChild( renderer.domElement );
 
 //render cube
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
+var cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+var cubeMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+//render sphere
+function makeSphere() {
+	var sphereGeometry = new THREE.SphereGeometry(0.5,32,32);
+	var sphereMaterial = new THREE.MeshBasicMaterial({color:0xffff00});
+	var sphere = new THREE.Mesh(sphereGeometry,sphereMaterial);
+
+	return sphere;
+}
+sphere = makeSphere();
+sphere2 = makeSphere();
+
+//add child sphere to cube
+cube.add(sphere);
+cube.add(sphere2)
+
+//add cube to scene
 scene.add( cube );
+
+
+var testRobot = new Robot();
 
 camera.position.z = 5;
 
-m = new THREE.Matrix4();
 m1 = new THREE.Matrix4();
 m2 = new THREE.Matrix4();
+m3 = new THREE.Matrix4();
+m4 = new THREE.Matrix4();
 
 m1.makeTranslation(3,0,0);
-// m2.makeRotationX(2)
-m.makeRotationY(0.01)
+m3.makeRotationY(0.01)
 m2.makeRotationZ(0.01)
-// m.multiplyMatrices(m1,m2);
+m4.makeTranslation(-1,0,0);
+
 cube.applyMatrix(m1)
+m1.makeTranslation(4,0,0);
+testRobot.body.applyMatrix(m1)
+
+m1.makeTranslation(1,0,0);
+
+sphere.applyMatrix(m1);
+sphere2.applyMatrix(m4);
 
 
 
-// cube.multiplyMatrices(m1,m);
 function render() {
 	requestAnimationFrame( render );
 	renderer.render( scene, camera );
-	// cube.rotation.x += 0.1;
-	// cube.rotation.y += 0.1;
 
 	cube.applyMatrix(m2)
-	cube.applyMatrix(m)
-	
+	cube.applyMatrix(m3)
+	// testRobot.body.applyMatrix(m2)
+	testRobot.body.applyMatrix(m3)
 
-	
-
-	// cube.quaternion = quaternion;
-	// cube.updateMatrix();
-
+	testRobot.walk();
 }
 render();
