14916e5283065969e4d8d8c0892fa15792ff0c8b
kirisanth <kirisanth.s@gmail.com>
2014-12-10 03:46:23 -0500

    Added plane, changed camera to orthographic camera, and removed old css files

--- js/3dobjects/test.js
+++ js/3dobjects/test.js
@@ -1,56 +1,56 @@
  //lighting example
  // revolutions per second
-      var angularSpeed = 0.2; 
-      var lastTime = 0;
- 
-      // this function is executed on each animation frame
-      function animate(){
-        // update
-        var time = (new Date()).getTime();
-        var timeDiff = time - lastTime;
-        var angleChange = angularSpeed * timeDiff * 2 * Math.PI / 1000;
-        cube.rotation.y += angleChange;
-        lastTime = time;
- 
-        // render
-        renderer.render(scene, camera);
- 
-        // request new frame
-        requestAnimationFrame(function(){
-            animate();
-        });
-      }
- 
-      // renderer
-      var renderer = new THREE.WebGLRenderer();
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      document.getElementById('canvas').appendChild( renderer.domElement );
- 
-      // camera
-      var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-      camera.position.z = 500;
- 
-      // scene
-      var scene = new THREE.Scene();
-                
-      // cube
-      var cube = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshLambertMaterial({
-        color: 'blue' 
-      }));
-      cube.overdraw = true;
-      cube.rotation.x = Math.PI * 0.1;
-      scene.add(cube);
-      
-      // add subtle blue ambient lighting
-      var ambientLight = new THREE.AmbientLight(0x000044);
-      scene.add(ambientLight);
-      
-      // directional lighting
-      var directionalLight = new THREE.DirectionalLight(0xffffff);
-      directionalLight.position.set(1, 1, 1).normalize();
-      scene.add(directionalLight);
+var angularSpeed = 0.2; 
+var lastTime = 0;
 
-      var robot = new Robot();
- 
-      // start animation
+// this function is executed on each animation frame
+function animate(){
+  // update
+  var time = (new Date()).getTime();
+  var timeDiff = time - lastTime;
+  var angleChange = angularSpeed * timeDiff * 2 * Math.PI / 1000;
+  cube.rotation.y += angleChange;
+  lastTime = time;
+
+  // render
+  renderer.render(scene, camera);
+
+  // request new frame
+  requestAnimationFrame(function(){
       animate();
+  });
+}
+
+// renderer
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.getElementById('canvas').appendChild( renderer.domElement );
+
+// camera
+var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.z = 500;
+
+// scene
+var scene = new THREE.Scene();
+          
+// cube
+var cube = new THREE.Mesh(new THREE.CubeGeometry(200, 200, 200), new THREE.MeshLambertMaterial({
+  color: 'blue' 
+}));
+cube.overdraw = true;
+cube.rotation.x = Math.PI * 0.1;
+scene.add(cube);
+
+// add subtle blue ambient lighting
+var ambientLight = new THREE.AmbientLight(0x000044);
+scene.add(ambientLight);
+
+// directional lighting
+var directionalLight = new THREE.DirectionalLight(0xffffff);
+directionalLight.position.set(1, 1, 1).normalize();
+scene.add(directionalLight);
+
+var robot = new Robot();
+
+// start animation
+animate();
